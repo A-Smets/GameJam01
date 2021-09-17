4fd225135ab41741f67fb363eeb0b1b5f57cc8f1
A-Smets <asmets@student.heaj.be>
2021-09-17 22:25:48 +0200

    Finalized steam trap

--- Unity/Assets/Scripts/Traps/SteamTrap.cs
+++ Unity/Assets/Scripts/Traps/SteamTrap.cs
@@ -5,22 +5,32 @@ using Sirenix.OdinInspector;
 
 public class SteamTrap : Trap
 {
+    [TitleGroup("Type Specific"), SerializeField] private Animator m_Animator;
+    [TitleGroup("Type Specific"), SerializeField] private string m_AnimatorActivate = "Activate";
+    [TitleGroup("Type Specific"), SerializeField] private string m_AnimatorPostActivate = "PostActivate";
     [TitleGroup("Type Specific"), SerializeField] private GameObject m_PreActivationVFX;
     [TitleGroup("Type Specific"), SerializeField] private GameObject m_ActivationVFX;
     [TitleGroup("Type Specific"), SerializeField] private GameObject m_PostActivationVFX;
+    [SerializeField] private GameObject m_YeetablePiece;
 
     protected override void SpringTrap(GameObject playerObj)
     {
-        //Valve anim
+        m_Animator.SetTrigger(m_AnimatorActivate);
         m_ActivationVFX.SetActive(true);
         m_PreActivationVFX.SetActive(false);
     }
 
     protected override void SetPostActivate()
     {
+        m_Animator.SetTrigger(m_AnimatorPostActivate);
         m_PreActivationVFX.SetActive(false);
         m_ActivationVFX.SetActive(false);
-        m_PostActivationVFX.SetActive(true);
-        //Remove valve
+        HideYeetable();
+        //m_PostActivationVFX.SetActive(true);
+    }
+
+    public void HideYeetable()
+    {
+        m_YeetablePiece.SetActive(false);
     }
 }
