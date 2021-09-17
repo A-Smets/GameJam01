ede545d9ea991ca0fba8d80bdce7ff95be86331f
A-Smets <asmets@student.heaj.be>
2021-09-17 11:56:14 +0200

    Update mushroom trap

--- Unity/Assets/Scripts/Traps/Trap.cs
+++ Unity/Assets/Scripts/Traps/Trap.cs
@@ -5,7 +5,7 @@ using Sirenix.OdinInspector;
 
 public abstract class Trap : MonoBehaviour
 {
-    [SerializeField, TitleGroup("Base Trap Data")] private BoolSO m_WasSprung;
+    [SerializeField, TitleGroup("Base Trap Data"), InfoBox("Unique BoolSO must be created for each trap", InfoMessageType = InfoMessageType.Warning)] private BoolSO m_WasSprung;
     [SerializeField, TitleGroup("Base Trap Data")] private Collider m_TriggerCollider;
     [SerializeField, TitleGroup("Base Trap Data")] private E_LayerCompare m_PlayerLayer = E_LayerCompare.Player;
 
---
+++ Unity/Assets/WIP/SteamTrap.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class SteamTrap : Trap
+{
+    [TitleGroup("Type Specific"), SerializeField] private GameObject m_PreActivationVFX;
+    [TitleGroup("Type Specific"), SerializeField] private GameObject m_ActivationVFX;
+    [TitleGroup("Type Specific"), SerializeField] private GameObject m_PostActivationVFX;
+
+    protected override void SpringTrap(GameObject playerObj)
+    {
+        //Valve anim
+        m_ActivationVFX.SetActive(true);
+        m_PreActivationVFX.SetActive(false);
+    }
+
+    protected override void SetToPostActivation()
+    {
+        m_PreActivationVFX.SetActive(false);
+        m_ActivationVFX.SetActive(false);
+        m_PostActivationVFX.SetActive(true);
+        //Remove valve
+    }
+}
